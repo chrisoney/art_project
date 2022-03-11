47f9240742906e8710f0d5f9de5fdd30834a175c
chrisoney <christopher.oney@yahoo.com>
2022-03-11 11:55:41 -0800

    start with button click event listener

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 import { lyrics } from './lyrics';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const newImageGenerateButton = document.getElementById('generate');
+
+  newImageGenerateButton.addEventListener('click', (e) => {
+    const randomLyricIndex = Math.floor(Math.random() * lyrics.length);
+    const rowIndex = Math.floor(Math.random());
+    const columnIndex = Math.floor(Math.random() * 10);
+
+    console.log(randomLyricIndex, rowIndex, columnIndex);
+  });
+});
+
